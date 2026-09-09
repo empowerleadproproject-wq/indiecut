b0c09084e78997a44e0f53e496ce909d06fa6098
empowerleadproproject-wq <empowerleadproproject@gmail.com>
2026-09-09 15:50:34 -0400

    Add TV category page

---
+++ app/tv/page.tsx
@@ -0,0 +1,3 @@
+import CategoryPage from '../CategoryPage';
+export const revalidate=0;
+export default function TVPage(){return <CategoryPage category="tv" title="TV" description="Television, streaming, series news, casting and standout performances."/>}
