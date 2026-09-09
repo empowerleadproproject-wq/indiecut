e959f07d7f42458a8cf72e9d622682dc0d243906
empowerleadproproject-wq <empowerleadproproject@gmail.com>
2026-09-09 15:50:26 -0400

    Add movies category page

---
+++ app/movies/page.tsx
@@ -0,0 +1,3 @@
+import CategoryPage from '../CategoryPage';
+export const revalidate=0;
+export default function MoviesPage(){return <CategoryPage category="movies" title="Movies" description="Film news, casting, releases, reviews and the people shaping the movies."/>}
