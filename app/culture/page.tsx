494a843083d29049824b9a771a971ff4f1f2d3fd
empowerleadproproject-wq <empowerleadproproject@gmail.com>
2026-09-09 15:50:47 -0400

    Add culture category page

---
+++ app/culture/page.tsx
@@ -0,0 +1,3 @@
+import CategoryPage from '../CategoryPage';
+export const revalidate=0;
+export default function CulturePage(){return <CategoryPage category="culture" title="Culture" description="The people, moments and conversations shaping entertainment and culture."/>}
