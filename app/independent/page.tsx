0de493dc1afedd3891aa2e439d4b0ba35a4b400b
empowerleadproproject-wq <empowerleadproproject@gmail.com>
2026-09-09 15:50:55 -0400

    Add independent category page

---
+++ app/independent/page.tsx
@@ -0,0 +1,3 @@
+import CategoryPage from '../CategoryPage';
+export const revalidate=0;
+export default function IndependentPage(){return <CategoryPage category="independent" title="Independent" description="Independent films, musicians, creators and emerging voices worth discovering."/>}
