a1c0f1bc12db883f02c02b908c9639c9abd48d13
empowerleadproproject-wq <empowerleadproproject@gmail.com>
2026-09-09 15:50:41 -0400

    Add music category page

---
+++ app/music/page.tsx
@@ -0,0 +1,3 @@
+import CategoryPage from '../CategoryPage';
+export const revalidate=0;
+export default function MusicPage(){return <CategoryPage category="music" title="Music" description="Artists, releases, performances, industry moves and the sounds shaping culture."/>}
