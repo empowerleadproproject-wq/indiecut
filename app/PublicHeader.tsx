b273ebef38bdda3b8a45c7b2f3f3217250d2f2ad
empowerleadproproject-wq <empowerleadproproject@gmail.com>
2026-09-09 15:55:58 -0400

    Remove Back Office from public navigation

--- app/PublicHeader.tsx
+++ app/PublicHeader.tsx
@@ -1,5 +1,5 @@
 export default function PublicHeader(){
- const links=[['/','HOME'],['/movies','MOVIES'],['/tv','TV'],['/music','MUSIC'],['/culture','CULTURE'],['/independent','INDEPENDENT'],['/articles','LATEST'],['/admin','BACK OFFICE']];
+ const links=[['/','HOME'],['/movies','MOVIES'],['/tv','TV'],['/music','MUSIC'],['/culture','CULTURE'],['/independent','INDEPENDENT'],['/articles','LATEST']];
  return <header className="ic-public-header">
   <div className="ic-public-masthead">
    <a className="ic-public-brand" href="/">INDIE CUT</a>
