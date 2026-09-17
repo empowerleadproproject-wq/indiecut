c315d017036c9d29bbbee52fdc805b2e9eec62c7
empowerleadproproject-wq <empowerleadproproject@gmail.com>
2026-09-17 18:17:20 -0400

    Wire room topic into share action

--- app/the-cut/layout.tsx
+++ app/the-cut/layout.tsx
@@ -1,10 +1,12 @@
 import RoomTitleFontSync from './RoomTitleFontSync';
 import RoomHostGate from './RoomHostGate';
 import HallwayRooms from './HallwayRooms';
+import RoomShareEnhancer from './RoomShareEnhancer';
 
 export default function TheCutLayout({children}:{children:React.ReactNode}){
   return <div className="the-cut-font-root">
     <RoomTitleFontSync/>
+    <RoomShareEnhancer/>
     <style>{`
       .the-cut-font-root main > header + section h1,
       .the-cut-font-root main article h2 {
