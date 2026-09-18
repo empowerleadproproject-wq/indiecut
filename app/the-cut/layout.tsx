e84d03d586b6e759a440a60d323abd87231b3e1d
empowerleadproproject-wq <empowerleadproproject@gmail.com>
2026-09-18 11:40:21 -0400

    Remove duplicate hallway manager mount

--- app/the-cut/layout.tsx
+++ app/the-cut/layout.tsx
@@ -1,6 +1,5 @@
 import RoomTitleFontSync from './RoomTitleFontSync';
 import RoomHostGate from './RoomHostGate';
-import HallwayRooms from './HallwayRooms';
 import RoomShareEnhancer from './RoomShareEnhancer';
 import CutBannerAds from './CutBannerAds';
 
@@ -16,7 +15,6 @@ export default function TheCutLayout({children}:{children:React.ReactNode}){
       }
     `}</style>
     <RoomHostGate>{children}</RoomHostGate>
-    <HallwayRooms/>
     <CutBannerAds/>
   </div>;
 }
